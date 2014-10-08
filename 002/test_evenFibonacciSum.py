e5b8962f917911aba51ba4e4612a95928dbb1027
kriegslustig <git@ls7.ch>
2014-10-08 16:19:55 +0200

    Fixing tha test

--- 002/test_evenFibonacciSum.py
+++ 002/test_evenFibonacciSum.py
@@ -4,17 +4,19 @@ from evenFibonacciSum import EvenFibonacciSum
 class TestEvenFibonacciSum(unittest.TestCase):
 	def setUp(self):
 		self.maxval = 100
-		self.evenFibonacciSum = EvenFibonacciSum(self.max)
+		self.evenFibonacciSum = EvenFibonacciSum(self.maxval)
 
 	def test_EvenFibonacciSum_calc(self):
-		self.assertTrue(type(evenFibonacciSum.calc()) == int)
+		self.assertTrue(type(self.evenFibonacciSum.calc()) == int)
 		self.assertRaises(TypeError, self.evenFibonacciSum.calc, [1,2,3])
 
 	def test_EvenFibonacciSum__getFibonacciTo(self):
-		self.assertTrue(type(evenFibonacciSum._getFibonacciTo()) == list)
-		lastTwo = ()
-		for n in evenFibonacciSum._getFibonacciTo():
-			if len(lastTwo == 2):
+		self.evenFibonacciSum__getFibonacciTo = self.evenFibonacciSum._getFibonacciTo()
+		self.assertTrue(type(self.evenFibonacciSum__getFibonacciTo) == list)
+		self.assertTrue(self.evenFibonacciSum__getFibonacciTo[0:5] == [1,2,3,5,8], 'What ever you did, doesn\'t seem to have much to do with fibonacci')
+		lastTwo = []
+		for n in self.evenFibonacciSum__getFibonacciTo:
+			if len(lastTwo) == 2:
 				self.assertTrue(sum(lastTwo) == n)
 				lastTwo[0] = lastTwo[1]
 				lastTwo[1] = n
