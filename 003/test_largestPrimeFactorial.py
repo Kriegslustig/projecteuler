95d27bb53e87f813067040c6f41b04894454a593
kriegslustig <git@ls7.ch>
2014-10-08 23:01:34 +0200

    Adding some documentation and refining some tests

--- 003/test_largestPrimeFactorial.py
+++ 003/test_largestPrimeFactorial.py
@@ -5,35 +5,35 @@ class TestLargestPrimeFactorial(unittest.TestCase):
 	def setUp(self):
 		self.largestPrimeFactorial = LargestPrimeFactorial()
 
-	def test_isPrime(self):
+	def test__isPrime(self):
 		'''
-			isPrime(7)
+			_isPrime(7)
 			This Method checks if a number is a prime
 		'''
 		msgFalse = 'This is not a prime: '
 		msgTrue = 'This is a prime: '
-		self.assertTrue(self.largestPrimeFactorial.isPrime(7), msgTrue + str(7))
-		self.assertFalse(self.largestPrimeFactorial.isPrime(15), msgFalse + str(15))
-		self.assertFalse(self.largestPrimeFactorial.isPrime(788), msgFalse + str(788))
+		self.assertTrue(self.largestPrimeFactorial._isPrime(7), msgTrue + str(7))
+		self.assertFalse(self.largestPrimeFactorial._isPrime(15), msgFalse + str(15))
+		self.assertFalse(self.largestPrimeFactorial._isPrime(4), msgFalse + str(4))
 
-	def test_calcPrimesTo(self):
+	def test__calcPrimesTo(self):
 		'''
-			calcPrimesTo(20)
-			uses isPrime
-			This Mehtod calculates all the primes smaller than half of the input 
+			_calcPrimesTo(20)
+			uses _isPrime
+			This Mehtod calculates all the primes smaller than half of the input
 		'''
-		msg = 'calcPrimesTo(20) should return [3, 5, 7, 11, 13, 17]'
-		test_calcPrimesTo20 = self.largestPrimeFactorial.calcPrimesTo(20)
-		self.assertTrue(test_calcPrimesTo20 == [3, 5, 7, 11, 13, 17], msg)
+		msg = ' _calcPrimesTo(20) should return [3, 5, 7, 11, 13, 17, 19]'
+		test_calcPrimesTo20 = self.largestPrimeFactorial._calcPrimesTo(20)
+		self.assertTrue(test_calcPrimesTo20 == [3, 5, 7, 11, 13, 17, 19], msg)
 
-	def test_calcFactorsOf(self):
+	def test__calcFactorsOf(self):
 		'''
-			calcFactorsOf([1, 2, 5, 7], 20)
+			_calcFactorsOf([1, 2, 5, 7], 20)
 			This Method should takes a list of integers as the second argument, checks if the first argument is devisable by them and returns the ones that are.
 		'''
-		msg = 'calcFactorsOf([1, 2, 5, 7], 20) should return [1, 2, 5]'
-		test_calcFactorsOf20 = self.largestPrimeFactorial.calcFactorsOf([1, 2, 5, 7], 20)
-		self.assertTrue(test_calcFactorsOf20 == [1, 2, 5], msg)
+		msg = '_calcFactorsOf([1, 2, 5, 7], 20) should return [1, 2, 5]'
+		test__calcFactorsOf20 = self.largestPrimeFactorial._calcFactorsOf([1, 2, 5, 7], 20)
+		self.assertTrue(test__calcFactorsOf20 == [1, 2, 5], msg)
 
 	def test_calc(self):
 		'''
