f5eef41093cfa0916c150e8f921efd29aa9f0f5c
kriegslustig <git@ls7.ch>
2014-10-08 23:02:33 +0200

    Adding the _calcPrimesTo method

--- 003/largestPrimeFactorial.py
+++ 003/largestPrimeFactorial.py
@@ -2,11 +2,19 @@ class LargestPrimeFactorial(object):
 	def __init__(self):
 		pass
 
-	def isPrime(self, n):
+	def _isPrime(self, n):
 		i = 2
-		while i < (n / 2):
+		while i <= (n / 2):
 			if n % i == 0:
-				break
 				return False
 			i += 1
 		return True
+
+	def _calcPrimesTo(self, n):
+		returnVal = []
+		i = 3
+		while i < n:
+			if self._isPrime(i):
+				returnVal.append(i)
+			i += 1
+		return returnVal
