73921a42cc7f4cba0e06c74635590658ae712e42
kriegslustig <git@ls7.ch>
2014-10-08 18:00:29 +0200

    Fixing a bug in the test for 003

--- 003/test_largestPrimeFactorial.py
+++ 003/test_largestPrimeFactorial.py
@@ -6,40 +6,40 @@ class TestLargestPrimeFactorial(unittest.TestCase):
 		self.largestPrimeFactorial = LargestPrimeFactorial()
 
 	def test_isPrime(self):
-	'''
-		isPrime(7)
-		This Method checks if a number is a prime
-	'''
-		msgFalse = '{} is not a prime'
-		msgTrue = '{} is a prime'
-		self.assertTrue(self.largestPrimeFactorial.isPrime(7), msgTrue)
-		self.assertTrue(self.largestPrimeFactorial.isPrime(15), msgFalse)
-		self.assertTrue(self.largestPrimeFactorial.isPrime(788), msgFalse)
+		'''
+			isPrime(7)
+			This Method checks if a number is a prime
+		'''
+		msgFalse = 'This is not a prime: '
+		msgTrue = 'This is a prime: '
+		self.assertTrue(self.largestPrimeFactorial.isPrime(7), msgTrue + str(7))
+		self.assertFalse(self.largestPrimeFactorial.isPrime(15), msgFalse + str(15))
+		self.assertFalse(self.largestPrimeFactorial.isPrime(788), msgFalse + str(788))
 
 	def test_calcPrimesTo(self):
-	'''
-		calcPrimesTo(20)
-		uses isPrime
-		This Mehtod calculates all the primes smaller than half of the input 
-	'''
+		'''
+			calcPrimesTo(20)
+			uses isPrime
+			This Mehtod calculates all the primes smaller than half of the input 
+		'''
 		msg = 'calcPrimesTo(20) should return [3, 5, 7, 11, 13, 17]'
 		test_calcPrimesTo20 = self.largestPrimeFactorial.calcPrimesTo(20)
 		self.assertTrue(test_calcPrimesTo20 == [3, 5, 7, 11, 13, 17], msg)
 
 	def test_calcFactorsOf(self):
-	'''
-		calcFactorsOf([1, 2, 5, 7], 20)
-		This Method should takes a list of integers as the second argument, checks if the first argument is devisable by them and returns the ones that are.
-	'''
+		'''
+			calcFactorsOf([1, 2, 5, 7], 20)
+			This Method should takes a list of integers as the second argument, checks if the first argument is devisable by them and returns the ones that are.
+		'''
 		msg = 'calcFactorsOf([1, 2, 5, 7], 20) should return [1, 2, 5]'
 		test_calcFactorsOf20 = self.largestPrimeFactorial.calcFactorsOf([1, 2, 5, 7], 20)
 		self.assertTrue(test_calcFactorsOf20 == [1, 2, 5], msg)
 
 	def test_calc(self):
-	'''
-		calc(13185)
-		This Method is the sort of the factory of largestPrimeFactorial it first uses calcPrimesTo and with the result of that it executes calcFactorsOf. And at last it gets the highest value in the return of calcFactorsOf
-	'''
+		'''
+			calc(13185)
+			This Method is the sort of the factory of largestPrimeFactorial it first uses calcPrimesTo and with the result of that it executes calcFactorsOf. And at last it gets the highest value in the return of calcFactorsOf
+		'''
 		msg = 'calc(13185) should return 13185'
 		calc13185 = self.largestPrimeFactorial.calc(13185)
 		self.assertTrue(calc13185 == 13185, msg)
